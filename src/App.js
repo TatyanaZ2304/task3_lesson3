d6e2f5490abe3dd70b9acbafd0cd45220dce87cd
Tatyana <brillenium@gmail.com>
2021-05-16 20:07:08 +0300

    added new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+    count:0
+  }
+  }
+
+  handleClickAdd(){
+    this.setState({
+    count: this.state.count+1
+    })
+  }
+
+  handleClickSub(){
+    this.setState({
+    count: this.state.count-1
+    })
+  }
+
+render(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h2>{this.state.count}</h2>
+         <button onClick = {() => this.handleClickAdd()}>
+        +
+        </button>
+        <button onClick = {() => this.handleClickSub()}>
+        -
+        </button>
+       </div>
   );
 }
-
+}
 export default App;
